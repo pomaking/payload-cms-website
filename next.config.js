188687e97ce1ba2a87a471299204479be31ff772
David Spencer <spencer07@gmail.com>
2025-06-18 11:00:36 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -37,6 +37,12 @@ const localhost = process.env.NEXT_PUBLIC_IS_LIVE
       },
     ]
 
+const NEXT_PUBLIC_SERVER_URL = process.env.VERCEL_PROJECT_PRODUCTION_URL
+    ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
+    : undefined || process.env.NEXT_PUBLIC_SERVER_URL || 'http://localhost:3000'
+  
+  /** @type {import('next').NextConfig} */
+
 const nextConfig = withBundleAnalyzer({
   eslint: {
     ignoreDuringBuilds: true,
