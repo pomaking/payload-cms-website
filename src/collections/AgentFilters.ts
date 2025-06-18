2ff6d0fcb276e86705d71393d184315bafa74b14
David Spencer <spencer07@gmail.com>
2025-06-18 10:33:20 -0400

    Create AgentFilters.ts

---
+++ src/collections/AgentFilters.ts
@@ -0,0 +1,43 @@
+import type { CollectionConfig } from 'payload'
+
+import { isAdmin } from '../access/isAdmin'
+
+const Filter: (slug: string, label: string) => CollectionConfig = (slug, label) => {
+  return {
+    slug,
+    access: {
+      create: isAdmin,
+      delete: isAdmin,
+      read: () => true,
+      update: isAdmin,
+    },
+    admin: {
+      group: 'Agent Program',
+      useAsTitle: 'name',
+    },
+    fields: [
+      {
+        name: 'name',
+        type: 'text',
+        label: label + ' Label',
+        required: true,
+        unique: true,
+      },
+      {
+        name: 'value',
+        type: 'text',
+        admin: {
+          description: 'Must contain only lowercase letters, numbers, hyphens, and underscores',
+        },
+        label: 'Value',
+        required: true,
+        unique: true,
+      },
+    ],
+  }
+}
+
+export const Licenses = Filter('licenses', 'License')
+export const Languages = Filter('languages', 'Language')
+export const MSAS = Filter('msas', 'MSA')
+export const Sectors = Filter('sectors', 'Sector')
