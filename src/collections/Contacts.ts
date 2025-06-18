4df66cfc8911eff5cc2a21204a54547d18ac8a12
David Spencer <spencer07@gmail.com>
2025-06-18 10:25:35 -0400

    Create Contacts.ts

---
+++ src/collections/Contacts.ts
@@ -0,0 +1,168 @@
+import { isAdmin, isAdminFieldLevel } from '../access/isAdmin'
+import { CollectionConfig } from 'payload'
+import { slugField } from '../fields/slug'
+import { formatPreviewURL } from '../utilities/formatPreviewURL'
+import { revalidatePath } from 'next/cache'
+export const Contacts: CollectionConfig = {
+  slug: 'contacts',
+  labels: {
+    singular: 'Contact',
+    plural: 'Contacts',
+  },
+  versions: {
+    drafts: false,
+  },
+  admin: {
+    group: 'CRM',
+    useAsTitle: 'fullName',
+  },
+  access: {
+    create: isAdmin,
+    read: isAdmin,
+    update: isAdmin,
+    delete: isAdmin,
+  },
+  fields: [
+    {
+      name: 'fullName',
+      type: 'text',
+      admin: {
+        hidden: true, // hides the field from the admin panel
+      },
+      hooks: {
+        beforeChange: [
+          ({ siblingData }) => {
+            // ensures data is not stored in DB
+            delete siblingData['fullName']
+          }
+        ],
+        afterRead: [
+          ({ data }) => {  
+            return `${data.firstName} ${data.lastName}`;
+          }
+        ],
+      },
+    },
+    
+    
+    {
+      name: 'firstName',
+      type: 'text',
+      label: 'First Name',
+      required: true,
+    },
+	{
+      name: 'lastName',
+      type: 'text',
+      label: 'Last Name',
+      required: true,
+    },
+	{
+      name: 'phone',
+      type: 'text',
+	  label: 'Phone',
+    },
+    {
+      name: 'email',
+      type: 'email',
+      label: 'Contact Email',
+      required: true,
+    },
+	{
+      name: 'social',
+      type: 'array',
+      label: 'Social Media Links',
+      fields: [
+        {
+          type: 'row',
+          fields: [
+            {
+               name: 'platform',                      
+			   type: 'select',
+               label: 'Platform',
+               required: true,
+               admin: {
+                 width: '50%',
+               },
+               options: [
+                 {
+                   label: 'LinkedIn',
+                   value: 'linkedin',
+                 },
+                 {
+                   label: 'Twitter',
+                   value: 'twitter',
+                 },
+                 {
+                   label: 'Facebook',
+                   value: 'facebook',
+                 },
+                 {
+                   label: 'Instagram',
+                   value: 'instagram',
+                 },
+                 {
+                   label: 'YouTube',
+                   value: 'youtube',
+                 },
+                 {
+                   label: 'GitHub',
+                   value: 'github',
+                 },
+               ],
+            },
+            {
+              name: 'url',
+              type: 'text',
+              label: 'URL',
+              required: true,
+              admin: {
+                width: '50%',
+              },
+            },
+          ],
+        },
+      ],
+    },
+    {
+      name: 'contactType',
+      type: 'select',
+      options: [
+        {
+          label: 'Prospect',
+          value: 'prospect',
+        },
+        {
+          label: 'Client',
+          value: 'client',
+        },
+		{
+          label: 'Partner',
+          value: 'partner',
+        },
+		{
+          label: 'Agent',
+          value: 'agent',
+        },
+		{
+          label: 'Inactive',
+          value: 'inactive',
+        },
+      ],
+      defaultValue: 'prospect',
+      admin: {
+        position: 'sidebar',
+        description: 'Set to inactive to hide this partner from the directory.',
+      },
+    },
+    {
+        name: 'companies',
+        type: 'relationship',
+        admin: {
+          position: 'sidebar',
+        },
+        hasMany: true,
+        relationTo: 'companies',
+    },
+  ],
+}
