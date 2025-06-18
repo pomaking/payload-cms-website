c1cb784e8003b6f816fa9868791e97895751ea09
David Spencer <spencer07@gmail.com>
2025-06-18 15:24:32 -0400

    Update route.ts

--- src/app/(frontend)/api/sync-ch/route.ts
+++ src/app/(frontend)/api/sync-ch/route.ts
@@ -5,7 +5,7 @@ import fetchDiscord from '../../../../scripts/fetchDiscord'
 import fetchGitHub from '../../../../scripts/fetchGitHub'
 import syncToAlgolia from '../../../../scripts/syncToAlgolia'
 
-export const maxDuration = 300 // 5 mins (max on vercel pro plan)
+export const maxDuration = 60 // 5 mins (max on vercel pro plan)
 export const dynamic = 'force-dynamic'
 
 export async function GET(): Promise<NextResponse> {
